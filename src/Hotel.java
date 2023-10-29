742f6622b996c035aa42e86fcc943ed421a3c562
Harshal1031 <harshalmalviya@gmail.com>
2023-10-29 18:33:40 +0530

    Find Cheapest Hotel

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,8 +1,86 @@
 
 public class Hotel {
+private   String hotelName;
+private  int rating;
+private  int rateForRegularCustomerWeekDays;
+private  int rateForRewardCustomerWeekDays;	
+private   int rateForRegularCustomerWeekends;
+private  int rateForRewardCustomerWeekends;
+public Hotel(String hotelName, int rating, int rateForRegularCustomerWeekDays, int rateForRewardCustomerWeekDays,
+		int rateForRegularCustomerWeekends, int rateForRewardCustomerWeekends) {
+	super();
+	this.hotelName = hotelName;
+	this.rating = rating;
+	this.rateForRegularCustomerWeekDays = rateForRegularCustomerWeekDays;
+	this.rateForRewardCustomerWeekDays = rateForRewardCustomerWeekDays;
+	this.rateForRegularCustomerWeekends = rateForRegularCustomerWeekends;
+	this.rateForRewardCustomerWeekends = rateForRewardCustomerWeekends;
+}
+public String getHotelName() {
+	return hotelName;
+}
+public void setHotelName(String hotelName) {
+	this.hotelName = hotelName;
+}
+public int getRating() {
+	return rating;
+}
+public void setRating(int rating) {
+	this.rating = rating;
+}
+public int getRateForRegularCustomerWeekDays() {
+	return rateForRegularCustomerWeekDays;
+}
+public void setRateForRegularCustomerWeekDays(int rateForRegularCustomerWeekDays) {
+	this.rateForRegularCustomerWeekDays = rateForRegularCustomerWeekDays;
+}
+public int getRateForRewardCustomerWeekDays() {
+	return rateForRewardCustomerWeekDays;
+}
+public void setRateForRewardCustomerWeekDays(int rateForRewardCustomerWeekDays) {
+	this.rateForRewardCustomerWeekDays = rateForRewardCustomerWeekDays;
+}
+public int getRateForRegularCustomerWeekends() {
+	return rateForRegularCustomerWeekends;
+}
+public void setRateForRegularCustomerWeekends(int rateForRegularCustomerWeekends) {
+	this.rateForRegularCustomerWeekends = rateForRegularCustomerWeekends;
+}
+public int getRateForRewardCustomerWeekends() {
+	return rateForRewardCustomerWeekends;
+}
+public void setRateForRewardCustomerWeekemds(int rateForRewardCustomerWeekemds) {
+	this.rateForRewardCustomerWeekends = rateForRewardCustomerWeekemds;
+}	
+   
+public void displayHotelDetails() {
+	System.out.println("The Name of The hotel is :"+this.hotelName);
+	System.out.println("The Rating of The hotel is :"+this.rating);
+	System.out.println("The Rate for Weekdays for Regular Customer is :"+this.rateForRegularCustomerWeekDays);
+	System.out.println("The Rate for Weekdays for Reward Customer is :"+this.rateForRewardCustomerWeekDays);
+	System.out.println("The Rate for Weekends for Regular Customer is :"+this.rateForRegularCustomerWeekends);
+	System.out.println("The Rate for Weekends for Reward Customer is :"+this.rateForRewardCustomerWeekends);
+    System.out.println();
+}
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Hotel Reservation Program in Hotel Reservation System");
-	}
+           public int costForDay(int i,String s) {
+        	   if(s==null)
+        		   return 0;
+        	   if(s=="MONDAY"||s=="TUESDAY"||s=="WEDNESDAY"||s=="THURSDAY"||s=="FRIDAY") {
+        		  if(i==1) {
+        			  return this.rateForRegularCustomerWeekDays;
+        		  }else {
+        			  return this.rateForRewardCustomerWeekDays;
+        		  }
+        	   }else {
+        		   if(i==1) {
+         			  return this.rateForRegularCustomerWeekends;
+         		  }else {
+         			  return this.rateForRewardCustomerWeekends;
+         		  }  
+        	   }
+        	   
+			        	   
+           }
 
 }
---
+++ src/HotelReservationSystem.java
@@ -0,0 +1,56 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class HotelReservationSystem {
+    List<Hotel> hotelList=new ArrayList<Hotel>();
+   public void addHotel(Hotel hotel) {
+	   hotelList.add(hotel);
+   }
+
+	public static void main(String[] args) {
+           Hotel hotel1=new Hotel("Lakewood",3,110,80,90,80);
+           Hotel hotel2=new Hotel("Bridegewood",4,160,110,60,50);
+           Hotel hotel3=new Hotel("Ridgewood",5,220,100,150,40);
+           HotelReservationSystem hrs=new HotelReservationSystem();
+           hrs.addHotel(hotel1);
+           hrs.addHotel(hotel2);
+           hrs.addHotel(hotel3);
+           
+           
+           hrs.countMinimumRate(1, hrs.togetDay(), hrs.togetDay());
+	}
+	public String togetDay(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Year:- ");
+        int year=sc.nextInt();
+        System.out.print("Enter the Month:- ");
+        int month= sc.nextInt();
+        System.out.print("Enter the Day:- ");
+        int day=sc.nextInt();
+
+        LocalDate date1= LocalDate.of(year, month, day);
+        return String.valueOf(date1.getDayOfWeek());
+    }
+	
+	public void countMinimumRate(int i,String day1, String day2) {
+		int sum1=hotelList.get(0).costForDay(i, day1)+hotelList.get(0).costForDay(i, day2);
+		int sum2=hotelList.get(1).costForDay(i, day1)+hotelList.get(1).costForDay(i, day2);
+		int sum3=hotelList.get(2).costForDay(i, day1)+hotelList.get(2).costForDay(i, day2);
+		
+            if(sum1<sum2&&sum1<sum3) {
+            	System.out.println("The minimum rate for the hotel "+hotelList.get(0).getHotelName()+" and it is "+sum1+"$"+" and the Rating of Hotel is :"+hotelList.get(0).getRating());
+            }else if(sum2<sum3) {
+            	System.out.println("The minimum rate for the hotel "+hotelList.get(1).getHotelName()+" and it is "+sum2+"$"+" and the Rating of Hotel is :"+hotelList.get(1).getRating());
+
+            }else {
+            	System.out.println("The minimum rate for the hotel "+hotelList.get(2).getHotelName()+" and it is "+sum3+"$"+" and the Rating of Hotel is :"+hotelList.get(2).getRating());
+
+            }
+	}
+
+}
